2b70da0e159e3c4402390516bbd881920ddffa2b
biswasprasana001 <biswasprasana004@gmail.com>
2022-02-19 16:58:00 +0530

    Done With Debugging

--- 08-menu/setup/app.js
+++ 08-menu/setup/app.js
@@ -71,6 +71,14 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "bison steak",
+    category: "dinner",
+    price: 22.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
@@ -115,7 +123,7 @@ function displayMenuButtons() {
   );
   const categoryBtns = categories
     .map(function (category) {
-      return `<button class="filter-btn" type="button" data-id="all">${category}</button>
+      return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>
     `;
     })
     .join("");
