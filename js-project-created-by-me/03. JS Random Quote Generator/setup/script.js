513ecf03ffb69fab78df89c634776e4a2baa2405
biswasprasana001 <biswasprasana004@gmail.com>
2022-07-04 01:26:14 +0530

    Quote Generator Project Completed

--- js-project-created-by-me/03. JS Random Quote Generator/setup/script.js
+++ js-project-created-by-me/03. JS Random Quote Generator/setup/script.js
@@ -1,4 +1,3 @@
-//Object Array to hold quotes, sources, citaitons and years
 var quotes = [
   {
     quote:
@@ -181,48 +180,37 @@ var quotes = [
   },
 ];
 
-//Function to randomly select a quote value and return a random quote object from the quotes array
-function getRandomQuote() {
-  var randomNumber = Math.floor(Math.random() * quotes.length);
-  var randomQuote = quotes[randomNumber];
+const randomQuote = () => {
+  const randomNumber = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomNumber];
   return randomQuote;
-}
+};
 
-//Function to select random rgb color value
-function getRandomColor() {
-  var red = Math.floor(Math.random() * 256);
-  var green = Math.floor(Math.random() * 256);
-  var blue = Math.floor(Math.random() * 256);
-  var randomColor = "rgb(" + red + "," + green + "," + blue + ")";
+const randomColor = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  const randomColor = "rgb(" + red + "," + green + "," + blue + ")";
   return randomColor;
-}
+};
 
-//Function to call the getRandomQuote function and stores the returned quote object in a variable
-//Constructs a string containing the different properties of the quote object
-function printQuote() {
-  var quotes = getRandomQuote();
-  var quoteContainer = document.getElementById("quote-box");
-  var quoteString = `<p class="quote">${quotes.quote}</p><p class="source">${quotes.source}`;
-  if (quotes.citation) {
-    quoteString += `<span class="citation">${quotes.citation}</span>`;
+const printQuote = () => {
+  const quote = randomQuote();
+  const quoteContainer = document.querySelector(".quote-container");
+  let quoteString = `<p class="quote">"${quote.quote}"</p>      <p class="source"><span>${quote.source}</span>
+  `;
+  if (quote.citation) {
+    quoteString += `<span class="citation">${quote.citation}</span>`;
   }
-  if (quotes.year) {
-    quoteString += `<span class="year">${quotes.year}</span></p>`;
+  if (quote.year) {
+    quoteString += `<span class="year">${quote.year}</span></p>
+    `;
   } else {
-    quoteString += "</p>";
+    quoteString += `</p>`;
   }
   quoteContainer.innerHTML = quoteString;
+  document.body.style.backgroundColor = randomColor();
+};
 
-  //assigns random color value to document background color
-  document.body.style.backgroundColor = getRandomColor();
-}
-
-//Quote automatically refreshes every 15 seconds
-window.setInterval(function () {
-  printQuote();
-}, 15000);
-
-//Event listener on LoadQuote button to generate new quote
-document
-  .getElementById("loadQuote")
-  .addEventListener("click", printQuote, false);
+const button = document.querySelector(".nextQuote");
+button.addEventListener("click", printQuote);
